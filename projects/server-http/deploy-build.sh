8bd9eb404afab2f994496fcbb8b87b3bffcf4e22
Andrew Messier <andrewgmessier@gmail.com>
2020-07-12 10:47:54 -0400

    Improve build deploy script and fix npm pack output

--- projects/server-http/deploy-build.sh
+++ projects/server-http/deploy-build.sh
@@ -2,25 +2,33 @@
 
 # Script executed to prepare for production deploy.
 # Run from server-http project folder.
+echo 'Install & Prune'
 npm install
-
-npm run clean-modules
-npm run clean
-npm run build
+npm prune
 
 # Build shared library
+echo 'Build Shared Library'
 cd ../shared
 npm install
 npm run build-production
-SHARED_PATH=npm pack | tail -n 1
+SHARED_PATH=$(npm pack | tail -n 1)
+echo $SHARED_PATH
 
 # Build server library
+echo 'Build Server Library'
 cd ../server
 npm install
 npm run build-production
-SERVER_PATH=npm pack | tail -n 1
+SERVER_PATH=$(npm pack | tail -n 1)
+echo $SERVER_PATH
 
 # Install shared and server into server-http
+echo 'Install Libraries'
 cd ../server-http
-npm install $SHARED_PATH
-npm install $SERVER_PATH
+npm install ../shared/$SHARED_PATH
+npm install ../server/$SERVER_PATH
+
+# Build
+echo 'Build'
+npm run clean
+npm run build
