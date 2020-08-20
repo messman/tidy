8a246a28595c93bf2beb25d7973a8a42936685c5
Andrew Messier <andrewgmessier@gmail.com>
2020-08-20 00:41:52 -0400

    Update dependencies, fix build script to install correctly

--- projects/server-http/deploy-build.sh
+++ projects/server-http/deploy-build.sh
@@ -18,6 +18,7 @@ echo $SHARED_PATH
 echo 'Build Server Library'
 cd ../server
 npm install
+npm install ../shared/$SHARED_PATH --no-save
 npm run build-production
 SERVER_PATH=$(npm pack | tail -n 1)
 echo $SERVER_PATH
@@ -25,8 +26,7 @@ echo $SERVER_PATH
 # Install shared and server into server-http
 echo 'Install Libraries'
 cd ../server-http
-npm install ../shared/$SHARED_PATH
-npm install ../server/$SERVER_PATH
+npm install ../shared/$SHARED_PATH ../server/$SERVER_PATH --no-save
 
 # Build
 echo 'Build'
