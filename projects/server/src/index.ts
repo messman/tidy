0f54576cb1b10294987e7033e6ccb70fe40e62de
Andrew Messier <andrewgmessier@gmail.com>
2022-05-31 02:09:15 +0000

    Move server files into server-http

--- projects/server/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { TestSeed, createWeatherData } from './test/all';
-export { createContext, APIConfigurationContext, APIConfiguration } from './all/context';
-export { getAllForConfiguration, getAllTestForConfiguration, createWellsConfiguration, createConfigurationFor } from './all/all';
-export { RunFlags, defaultRunFlags } from './util/run-flags';
