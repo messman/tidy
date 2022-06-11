812909774d7adf0cfb877378668959735ce45080
Andrew Messier <andrewgmessier@gmail.com>
2022-06-11 14:36:54 +0000

    Update moon icons and add phase icon component

---
+++ projects/client/src/core/astro/moon-phase-icon.fixture.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { CosmosFixture } from '@/test';
+import { FixtureContainer } from '@/test/cosmos-fixture';
+import * as iso from '@wbtdevlocal/iso';
+import { MoonPhaseIcon } from './moon-phase-icon';
+
+export default CosmosFixture.create(() => {
+
+	return (
+		<>
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.a_new} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.b_waxingCrescent} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.c_firstQuarter} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.d_waxingGibbous} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.e_full} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.f_waningGibbous} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.g_thirdQuarter} />
+			<MoonPhaseIcon phase={iso.Astro.MoonPhase.h_waningCrescent} />
+		</>
+	);
+}, {
+	container: FixtureContainer.panelPadding
+});
---
+++ projects/client/src/core/astro/moon-phase-icon.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { icons } from '@wbtdevlocal/assets';
+import * as iso from '@wbtdevlocal/iso';
+import { Icon, IconInputType } from '../icon/icon';
+import { styled, StyledFC } from '../theme/styled';
+
+export interface MoonPhaseIconProps {
+	phase: iso.Astro.MoonPhase;
+}
+
+export const MoonPhaseIcon: StyledFC<MoonPhaseIconProps> = (props) => {
+	const { phase } = props;
+
+	const icon = iso.mapEnumValue(iso.Astro.MoonPhase, moonPhaseIcon, phase);
+
+	return (
+		<MoonPhaseIconContainer>
+			<Icon type={icon} />
+		</MoonPhaseIconContainer>
+	);
+};
+
+const MoonPhaseIconContainer = styled.span`
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	background-color: ${p => p.theme.common.content.backgroundNight};
+	border-radius: 50%;
+	width: 2rem;
+	height: 2rem;
+	// Slightly smaller in comparison to weather icons; because moon takes up all space as a circle
+	padding: .125rem;
+`;
+
+const moonPhaseIcon: Record<keyof typeof iso.Astro.MoonPhase, IconInputType> = {
+	a_new: icons.moonNew,
+	b_waxingCrescent: icons.moonWaxingCrescent,
+	c_firstQuarter: icons.moonFirstQuarter,
+	d_waxingGibbous: icons.moonWaxingGibbous,
+	e_full: icons.moonFull,
+	f_waningGibbous: icons.moonWaningGibbous,
+	g_thirdQuarter: icons.moonThirdQuarter,
+	h_waningCrescent: icons.moonWaningCrescent,
+};
